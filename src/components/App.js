be8b3546b00314b91fe14ad185af7682a1eabcc8
MsWakesho <wakeshosonja@gmail.com>
2024-02-04 07:22:56 +0300

    Completed Assignment

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EyesOnMe from "../components/EyesOnMe"
+import Keypad from "./Keypad";
+
+function App (){
+    return(
+        <div>
+        <EyesOnMe />
+        <Keypad />
+        </div>
+    )
+}
+
+export default App
